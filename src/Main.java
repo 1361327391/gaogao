24b9e377ee4da6df9be78b0d746f615a2d8ebf0e
YitianGao <1361327391@qq.com>
2020-12-10 17:21:29 +0800

    IDEA

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld!");
-        System.out.println("HelloWorld!");
-        System.out.println("HelloWorld!");dfdsfdsfd
-        System.out.println("fjghfjkdsfbdskjfb");
-        System.out.dsfdsfdsfdsfdsfdsfprintln("HelloWorld!");
-        System.out.pdsfdsfdsfrintln("H" +
-                "" +
-                "fdsfdsfdsfdsfsdelloWorld!");
-        System.out.println("HelloWorld!");
+
+        System.out.println("fsdfsdf");
     }
 }
